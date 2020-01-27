6943d2d5c2039cfdf22a4e3081928821f52aa72e
vitor.rozsa <vitor.rozsa@hotmail.com>
2020-01-27 06:31:39 -0300

    Update wearable holder fields

--- src/main/java/com/rozsa/model/NpcWearableHolder.java
+++ src/main/java/com/rozsa/model/NpcWearableHolder.java
@@ -7,6 +7,26 @@ public class NpcWearableHolder {
 
     private int defense;
 
+    private int minRange;
+
+    private int maxRange;
+
+    public int getMinRange() {
+        return minRange;
+    }
+
+    public void setMinRange(int minRange) {
+        this.minRange = minRange;
+    }
+
+    public int getMaxRange() {
+        return maxRange;
+    }
+
+    public void setMaxRange(int maxRange) {
+        this.maxRange = maxRange;
+    }
+
     public int getMinAttack() {
         return minAttack;
     }
