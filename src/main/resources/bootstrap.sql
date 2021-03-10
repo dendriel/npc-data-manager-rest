6aa6f412a55a9871f6f652471cf625fbc3aa13b9
vitor.rozsa <vitor.rozsa@hotmail.com>
2021-03-10 19:52:30 -0300

    Update compose file

--- src/main/resources/bootstrap.sql
+++ src/main/resources/bootstrap.sql
@@ -1,5 +1,8 @@
-# must be executed manually once to bootstrap the service.
-
-INSERT INTO directory (name) value('default', 'idStorage');
+-- must be executed manually once to bootstrap the service.
 
 INSERT INTO system_properties (config, val) value('storage.bucket', 'npcdatamanager');
+
+INSERT INTO directory (name, storage_id) values ("default", "images");
+INSERT INTO resource (name, storage_id, type, directory_id) values ("Iron Dagger", "images/icon_iron_dagger.png", 1, 1);
+INSERT INTO resource (name, storage_id, type, directory_id) values ("Merchant 01", "images/merchant01.png", 1, 1);
+INSERT INTO resource (name, storage_id, type, directory_id) values ("Bandit 01", "images/bandit01.png", 1, 1);
